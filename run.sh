fd8c6abaf119b84bbee681ce96e530db5301297c
Eric106 <ercv106@hotmail.com>
2023-05-17 00:24:58 -0600

    Wed 17 May 2023 12:24:58 AM CST

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+path_script="$(cd "$(dirname "$0")" && pwd)"
+
 tmux new-session -d -s http2https
-tmux send-keys -t http2https.0 "cd /home/eric/Documents/http2https/ " ENTER
+tmux send-keys -t http2https.0 "cd $path_script " ENTER
 tmux send-keys -t http2https.0 "conda activate http2https " ENTER
 tmux send-keys -t http2https.0 "bash ./start_gunicorn.sh " ENTER
