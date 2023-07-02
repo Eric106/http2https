8bee83f1168ca0ebf5f6013589ac51984ab77afa
Eric106 <ercv106@hotmail.com>
2023-07-02 01:03:35 -0600

    Sun 02 Jul 2023 01:03:35 AM CST

--- http2https.py
+++ http2https.py
@@ -1,5 +1,5 @@
 from modules.proxy import app
 
 if __name__ == '__main__':
-    print('starting WSGI server at https://localhost:8080')
-    app.run(host='0.0.0.0', port=8080)
+    print('starting WSGI server at https://localhost:8000')
+    app.run(host='0.0.0.0', port=8000)
