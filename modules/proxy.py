4f0046e6b82a1b11ebd417a611980a0b638600fb
Eric106 <ercv106@hotmail.com>
2024-05-06 18:36:54 -0600

    Mon 06 May 2024 06:36:54 PM CST

--- modules/proxy.py
+++ modules/proxy.py
@@ -1,5 +1,5 @@
 # from pprint import pprint
-from flask import Flask, request
+from flask import Flask, Response, request
 import requests
 from json import load
 
@@ -12,27 +12,26 @@ def json2dict(path_json: str) -> dict:
 app = Flask(__name__)
 bind_config = json2dict('bind.json')
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def proxy(path):
-    http_ip = bind_config['ip']
-    http_port = bind_config['http_port']
-    url = f'http://{http_ip}:{http_port}/{path}'
-    # print('='*50)
-    # pprint(request.headers)
-    try:
-        response = requests.request(
-            method=request.method,
-            url=url,
-            headers={key: value for (key, value) in request.headers},
-            data=request.get_data(),
-            cookies=request.cookies,
-            allow_redirects=False  # Prevent the target service from redirecting
-        )
-    except Exception:
-        return "503 Iternal Server Error", 503
+@app.route('/', defaults={'path': ''}, methods=["GET", "POST"])  # ref. https://medium.com/@zwork101/making-a-flask-proxy-server-online-in-10-lines-of-code-44b8721bca6
+@app.route('/<path:path>', methods=["GET", "POST"])  # NOTE: better to specify which methods to be accepted. Otherwise, only GET will be accepted. Ref: https://flask.palletsprojects.com/en/3.0.x/quickstart/#http-methods
+def proxy(path):  #NOTE var :path will be unused as all path we need will be read from :request ie from flask import request
+    url = f"http://{bind_config['ip']}:{bind_config['http_port']}/"
+    res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+        method          = request.method,
+        url             = request.url.replace(request.host_url, url),
+        headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
+        data            = request.get_data(),
+        cookies         = request.cookies,
+        allow_redirects = False,
+    )
 
-    # print('-'*50)
-    # pprint(response.headers)
-    # print('='*50)
-    return response.content, response.status_code, dict(response.headers)
+    #region exlcude some keys in :res response
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding','hop-by-hop', 'connection']  #NOTE we here exclude all "hop-by-hop headers" defined by RFC 2616 section 13.5.1 ref. https://www.rfc-editor.org/rfc/rfc2616#section-13.5.1
+    headers          = [
+        (k,v) for k,v in res.raw.headers.items()
+        if k.lower() not in excluded_headers
+    ]
+    #endregion exlcude some keys in :res response
+
+    response = Response(res.content, res.status_code, headers)
+    return response
